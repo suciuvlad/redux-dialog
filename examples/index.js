81d775eef3f3e7e1d8ce174a8a4472b9ae71e1de
Vlad Suciu <suciu.vlad@gmail.com>
2018-06-19 17:34:31 +0300

    Fix example issue: undefined props

--- examples/index.js
+++ examples/index.js
@@ -19,7 +19,7 @@ const store = createStore(reducer);
 const BasicDialog = props => (
   <div>
     <div className="dlg--body">
-      My awesome modalbox for "{props.payload.contentId} "!{' '}
+      My awesome modalbox for `${props.payload && props.payload.contentId}`
       <a href="#" onClick={props.onRequestClose}>
         Close it
       </a>
